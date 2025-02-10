7c19c2b5d7355254aff9e4c3b919cc8cd8d11197
Radomir Skrzepij <zyziu10@gmail.com>
2025-02-10 17:05:21 +0100

    add interface and remove unneded code

--- src/utils/sectionUtils.ts
+++ src/utils/sectionUtils.ts
@@ -1,9 +1,25 @@
 import { getCollection } from 'astro:content';
 
-export async function getSections(currentLang: string) {
+interface GetSections {
+    section1: {
+        imageSrc: string;
+        text: string;
+    };
+    section2: {
+        imageSrc: string;
+        text: string;
+    };
+    section3: {
+        imageSrc: string;
+        text: string;
+    };
+}
+
+export async function getSections(currentLang: string): Promise<GetSections> {
     const section1 = await getCollection('sections', (entry) => entry.id === `section1.${currentLang}.md`);
     const section2 = await getCollection('sections', (entry) => entry.id === `section2.${currentLang}.md`);
     const section3 = await getCollection('sections', (entry) => entry.id === `section3.${currentLang}.md`);
+
     return {
         section1: section1[0].data,
         section2: section2[0].data,
